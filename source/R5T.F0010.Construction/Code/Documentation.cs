7a0c8c42c4d2ce6a5ed48f1baa19a1c27fd20d07
davidcoats <david@getsafetycone.com>
2022-05-09 13:44:32 -0700

    Initial commit of source-stub.

---
+++ source/R5T.F0010.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.F0010.Construction
+{
+    /// <summary>
+    /// Construction project for One-stop-shop aggregation library for all Roslyn syntax element operations..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.F0010/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.F0010
+{
+    /// <summary>
+    /// One-stop-shop aggregation library for all Roslyn syntax element operations.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
